518c608fdf119063622a8c0f8b80c5f7a79dce89
agustindemiddi <agustindemiddi@gmail.com>
2023-10-31 12:04:13 -0300

    add DUMMY_BACKEND.js

---
+++ src/DUMMY/DUMMY_BACKEND.js
@@ -0,0 +1,69 @@
+const DUMMY_IMAGE =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxJNdns8vLpqrBm4VOpORyvS0bh8bjKy9M5-A_t_upDYHFxAn3DwlbhqjbDUkpk2Thock&usqp=CAU';
+
+export const tournaments = [
+  {
+    id: 't1',
+    name: 'Pepito Cup',
+    description: 'lorem-ipsum',
+    players: [],
+    image: DUMMY_IMAGE,
+  },
+  {
+    id: 't2',
+    name: 'Cornalito Cup',
+    description: '',
+    players: [],
+    image: DUMMY_IMAGE,
+  },
+  {
+    id: 't3',
+    name: `Bowser's Cup`,
+    description: '',
+    players: [],
+    image: DUMMY_IMAGE,
+  },
+  {
+    id: 't4',
+    name: 'Apertura Tournament',
+    description: 'lorem-ipsum',
+    players: [],
+    image: DUMMY_IMAGE,
+  },
+];
+
+export const groups = [
+  {
+    id: 'g1',
+    name: 'The Pibes',
+    players: [],
+    image: DUMMY_IMAGE,
+  },
+  {
+    id: 'g2',
+    name: 'The Wolfpack',
+    players: [],
+    image: DUMMY_IMAGE,
+  },
+];
+
+export const players = [
+  {
+    id: 'p1',
+    name: 'Carlos Maslatón',
+    description: '',
+    image: DUMMY_IMAGE,
+  },
+  {
+    id: 'p2',
+    name: 'Andy Chango',
+    description: '',
+    image: DUMMY_IMAGE,
+  },
+  {
+    id: 'p3',
+    name: 'Pepino Juárez',
+    description: '',
+    image: DUMMY_IMAGE,
+  },
+];
