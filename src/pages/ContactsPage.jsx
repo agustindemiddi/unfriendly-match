ab92f541c73da50b0146a7a0e8be9303621e15d1
agustindemiddi <agustindemiddi@gmail.com>
2023-11-04 11:30:23 -0300

    add utils/firebaseConfig.js to initialize database

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,5 +1,14 @@
+import { collection, getDocs } from 'firebase/firestore';
+
 import PageContent from '../components/UI/PageContent';
 import Dashboard from '../components/Dashboard/Dashboard';
+
+import db from '../utils/firebaseConfig';
+
+const querySnapshot = await getDocs(collection(db, 'users'));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data().name}`);
+});
 
 import DUMMY_BACKEND from '../DUMMY/DUMMY_BACKEND';
 const section = [];
---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+// import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {};
+if (import.meta.env.MODE === 'development') {
+  (firebaseConfig.apiKey = import.meta.env.VITE_API_KEY),
+    (firebaseConfig.authDomain = import.meta.env.VITE_AUTH_DOMAIN),
+    (firebaseConfig.projectId = import.meta.env.VITE_PROJECT_ID),
+    (firebaseConfig.storageBucket = import.meta.env.VITE_STORAGE_BUCKET),
+    (firebaseConfig.messagingSenderId =
+      import.meta.env.VITE_MESSAGING_SENDER_ID),
+    (firebaseConfig.appId = import.meta.env.VITE_APP_ID),
+    (firebaseConfig.measurementId = import.meta.env.VITE_MEASUREMENT_ID);
+}
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+// const analytics = getAnalytics(app);
+
+export default db;
